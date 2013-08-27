40fa2064f104f8786fa7250a9f5215b087113969
Stefan Judis <stefan.judis@project-a.com>
2013-08-27 16:54:04 +0200

    added htmlmin task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,26 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'dist/index.html': 'index.html'     // 'destination': 'source'
+        }
+      }
+    }
   });
 
 
+  // load custom tasks
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask( 'default', []);
+  grunt.registerTask( 'dist', [ 'htmlmin' ] );
 
 };
